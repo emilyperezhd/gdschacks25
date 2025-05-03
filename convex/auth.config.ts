e8a92fe0861f5cebd2810e122c40245051c7cf2f
Ivan Sostaric <ivansostaric64@gmail.com>
2025-05-03 12:58:14 -0400

    Got the sign in database working

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://peaceful-baboon-38.clerk.accounts.dev/",
+      domain: "https://above-mako-56.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
